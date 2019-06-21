ab065174f1c676805289eefdc200fe8f0ad1c123
Logan Prust <ljprust@uwm.edu>
2019-06-21 13:23:06 -0500

    working 2D code

--- main2.py
+++ main2.py
@@ -14,9 +14,9 @@ parser.add_argument('--mirror', action='store_true')
 args = parser.parse_args()
 
 # set parameters
-nCellsX     = 100
-nCellsY     = 100
-nSteps      = 500
+nCellsX     = 10
+nCellsY     = 10
+nSteps      = 40
 mirrorCellX = 50
 mirrorCellY = 50
 downsample  = 10
@@ -41,16 +41,14 @@ P[ cellRight:nCellsX, : ] = P2
 rho = np.ones((nCellsX, nCellsY)) * rho1
 rho[ cellRight:nCellsX, : ] = rho2
 vx = np.ones((nCellsX, nCellsY)) * v1
-vx[ cellRight:nCells, : ] = v2
+vx[ cellRight:nCellsX, : ] = v2
 vy = np.zeros((nCellsX, nCellsY))
 
 # set positions and widths
-deltax = np.ones(nCellsX) * boxSizeX / float(nCellsX)
-dx = deltax[0]
-x = range(0,nCellsX) * dx + 0.5 * dx
-deltay = np.ones(nCellsY) * boxSizeY / float(nCellsY)
-dy = deltay[0]
-y = range(0,nCellsY) * dy + 0.5 * dy
+dx = boxSizeX / float(nCellsX)
+x = ( np.arange(0,nCellsX) + 0.5 ) * dx
+dy = boxSizeY / float(nCellsY)
+y = ( np.arange(0,nCellsY) + 0.5 ) * dy
 
 # preallocate some arrays
 cons1 = np.zeros(nSteps)
@@ -78,7 +76,7 @@ def getE(P, gamma, rho, vx, vy) :
     E = rho * ( e + 0.5 * ( vx*vx + vy*vy ) )
     return E
 
-def buildU(rho, vx, vy, P) :
+def buildU(rho, vx, vy, P, gamma) :
     shape = rho.shape
     nx = shape[0]
     ny = shape[1]
@@ -87,7 +85,7 @@ def buildU(rho, vx, vy, P) :
     U[0,0:nx,0:ny] = rho
     U[1,0:nx,0:ny] = rho * vx
     U[2,0:nx,0:ny] = rho * vy
-    U[2,0:nx,0:ny] = E
+    U[3,0:nx,0:ny] = E
     return U
 
 def buildFcent(rho, vx, vy, P) :
@@ -124,9 +122,9 @@ def getCons(U) :
 def splitScalarX(array) :
     shape = array.shape
     nx = shape[0]
-    arrayL = array[0:nx-1, :]
-    arrayR = array[1:nx, :]
-    return arrayL, arrayR
+    arrayB = array[0:nx-1, :]
+    arrayT = array[1:nx, :]
+    return arrayB, arrayT
 
 def splitScalarY(array) :
     shape = array.shape
@@ -145,9 +143,9 @@ def splitVectorX(array) :
 def splitVectorY(array) :
     shape = array.shape
     ny = shape[2]
-    arrayL = array[:, :, 0:ny-1]
-    arrayR = array[:, :, 1:ny]
-    return arrayL, arrayR
+    arrayB = array[:, :, 0:ny-1]
+    arrayT = array[:, :, 1:ny]
+    return arrayB, arrayT
 
 def getc(gamma, P, rho) :
     c = np.sqrt( gamma * P / rho )
@@ -183,9 +181,51 @@ def splitReconY(var) :
     varp2 = var[:, 3:ny]
     return varm1, var0, varp1, varp2
 
-def Riemann(U, gamma, deltax, deltay) :
-    nCellsX = len(deltax)
-    nCellsY = len(deltay)
+def flipVelX(U) :
+    U[1,:] = -U[1,:]
+    return U
+
+def flipVelY(U) :
+    U[2,:] = -U[2,:]
+    return U
+
+def getEdgeStates(U) :
+    shape = U.shape
+    nx = shape[1]
+    ny = shape[2]
+
+    UL = U[:, 0, :]
+    UR = U[:, nx-1, :]
+    UB = U[:, :, 0]
+    UT = U[:, :, ny-1]
+
+    ULflip = flipVelX(UL)
+    URflip = flipVelX(UR)
+    UBflip = flipVelY(UB)
+    UTflip = flipVelY(UT)
+
+    UL2 = np.zeros((4, 2, ny))
+    UL2[:, 0, :] = ULflip
+    UL2[:, 1, :] = UL
+
+    UR2 = np.zeros((4, 2, ny))
+    UR2[:, 0, :] = UR
+    UR2[:, 1, :] = URflip
+
+    UB2 = np.zeros((4, nx, 2))
+    UB2[:, :, 0] = UBflip
+    UB2[:, :, 1] = UB
+
+    UT2 = np.zeros((4, nx, 2))
+    UT2[:, :, 0] = UT
+    UT2[:, :, 1] = UTflip
+
+    return UL2, UR2, UB2, UT2
+
+def getFlux(U, gamma) :
+    shape = U.shape
+    nCellsX = shape[1]
+    nCellsY = shape[2]
 
     # extract state variables
     rho, vx, vy, P = getState(U, gamma)
@@ -195,10 +235,10 @@ def Riemann(U, gamma, deltax, deltay) :
     Gcent = buildGcent(rho, vx, vy, P)
 
     # split into left and right values
-    ULX, URX = splitVectorX(U)
-    ULY, URY = splitVectorY(U)
+    UL, UR = splitVectorX(U)
+    UB, UT = splitVectorY(U)
     FcentL, FcentR = splitVectorX(Fcent)
-    GcentL, GcentR = splitVectorY(Gcent)
+    GcentB, GcentT = splitVectorY(Gcent)
 
     # get sound speed
     c = getc(gamma, P, rho)
@@ -208,34 +248,65 @@ def Riemann(U, gamma, deltax, deltay) :
     lambdaMx = vx - c
     lambdaPy = vy + c
     lambdaMy = vy - c
-    lambdaPx_L, lambdaPx_R = splitScalar(lambdaPx)
-    lambdaMx_L, lambdaMx_R = splitScalar(lambdaMx)
-    lambdaPy_L, lambdaPy_R = splitScalar(lambdaPy)
-    lambdaMy_L, lambdaMy_R = splitScalar(lambdaMy)
+    lambdaPx_L, lambdaPx_R = splitScalarX(lambdaPx)
+    lambdaMx_L, lambdaMx_R = splitScalarX(lambdaMx)
+    lambdaPy_B, lambdaPy_T = splitScalarY(lambdaPy)
+    lambdaMy_B, lambdaMy_T = splitScalarY(lambdaMy)
     alphaPx = max3( lambdaPx_L, lambdaPx_R )
     alphaMx = max3( -lambdaMx_L, -lambdaMx_R )
-    alphaPy = max3( lambdaPy_L, lambdaPy_R )
-    alphaMy = max3( -lambdaMy_L, -lambdaMy_R )
+    alphaPy = max3( lambdaPy_B, lambdaPy_T )
+    alphaMy = max3( -lambdaMy_B, -lambdaMy_T )
     alphaMaxX = np.maximum( alphaPx.max(), alphaMx.max() )
     alphaMaxY = np.maximum( alphaPy.max(), alphaMy.max() )
     alphaMax = np.maximum(alphaMaxX, alphaMaxY)
 
     # find fluxes at faces
-    Fface = np.zeros([ 4, nCellsX+1 ])
-    Gface = np.zeros([ 4, nCellsY+1 ])
-    Fface[:,1:nCellsX] = ( alphaPx * FcentL + alphaMx * FcentR - alphaPx * alphaMx * (URX - ULX) ) / ( alphaPx + alphaMx )
-    FfaceL, FfaceR = splitVector(Fface)
-    Gface[:,1:nCellsY] = ( alphaPy * GcentL + alphaMy * GcentR - alphaPy * alphaMy * (URY - ULY) ) / ( alphaPy + alphaMy )
-    GfaceL, GfaceR = splitVector(Gface)
+    Fface = ( alphaPx * FcentL + alphaMx * FcentR - alphaPx * alphaMx * (UR - UL) ) / ( alphaPx + alphaMx )
+    Gface = ( alphaPy * GcentB + alphaMy * GcentT - alphaPy * alphaMy * (UT - UB) ) / ( alphaPy + alphaMy )
+
+    return Fface, Gface, alphaMax
+
+def getL(U, gamma, dx, dy) :
+    shape = U.shape
+    nCellsX = shape[1]
+    nCellsY = shape[2]
+
+    # get flux on interior faces
+    FfaceI, GfaceI, alphaMaxI = getFlux(U, gamma)
+
+    # get flux on edges
+    UL, UR, UB, UT = getEdgeStates(U)
+    FfaceL, GfaceL, alphaMaxL = getFlux(UL, gamma)
+    FfaceR, GfaceR, alphaMaxR = getFlux(UR, gamma)
+    FfaceB, GfaceB, alphaMaxB = getFlux(UB, gamma)
+    FfaceT, GfaceT, alphaMaxT = getFlux(UT, gamma)
+
+    # find the overall largest alpha
+    alphaMax = max( [alphaMaxI, alphaMaxL, alphaMaxR, alphaMaxB, alphaMaxT] )
+
+    # construct the full flux arrays
+    FfaceFull = np.zeros((4, nCellsX+1, nCellsY))
+    GfaceFull = np.zeros((4, nCellsX, nCellsY+1))
+
+    FfaceFull[:, 1:nCellsX, :] = FfaceI
+    GfaceFull[:, :, 1:nCellsY] = GfaceI
+    FfaceFull[:, 0, :]         = FfaceL[:,0,:]
+    FfaceFull[:, nCellsX, :]   = FfaceR[:,0,:]
+    GfaceFull[:, :, 0]         = GfaceB[:,:,0]
+    GfaceFull[:, :, nCellsY]   = GfaceT[:,:,0]
+
+    # split flux arrays
+    FfaceFullL, FfaceFullR = splitVectorX(FfaceFull)
+    GfaceFullB, GfaceFullT = splitVectorY(GfaceFull)
 
     # find time derivatives
-    L = - ( FfaceR - FfaceL ) / deltax - ( GfaceR - GfaceL ) / deltay
+    L = - ( FfaceFullR - FfaceFullL ) / dx - ( GfaceFullT - GfaceFullB ) / dy
 
     return L, alphaMax
 
 # initialize t, U
 t = 0.0
-U = buildU(rho, vx, vy, P)
+U = buildU(rho, vx, vy, P, gamma)
 rho, vx, vy, P = getState(U, gamma)
 
 # save variables for animation
@@ -257,11 +328,11 @@ for i in range(0,nSteps) :
     # if args.recon :
     #     L, alphaMax = RiemannRecon(U, gamma, deltax)
     # else :
-    L, alphaMax = Riemann(U, gamma, deltax)
+    L, alphaMax = getL(U, gamma, dx, dy)
 
     # find timestep
-    deltatx = courantFac * deltax / alphaMax
-    deltaty = courantFac * deltay / alphaMax
+    deltatx = courantFac * dx / alphaMax
+    deltaty = courantFac * dy / alphaMax
     minStepX = deltatx.min()
     minStepY = deltaty.min()
     minStep = np.minimum(minStepX, minStepY)
@@ -293,3 +364,8 @@ for i in range(0,nSteps) :
     tAnim[i+1] = t
 
 stdout.write('\nDone crunching numbers\n')
+
+print('rho ',rho)
+print('vx ',vx)
+print('vy ',vy)
+print('P ',P)
